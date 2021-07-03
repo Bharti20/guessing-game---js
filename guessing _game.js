409447c19a0bbe8cba1986d4ab2183e32055942f
Bharti kumari <bhartikumari20@navgurukul.org>
2021-07-03 19:58:25 +0530

    guessing game in js

---
+++ guessing _game.js
@@ -0,0 +1,20 @@
+const input = require('readline-sync')
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+let computerSelect = numbers[Math.floor(Math.random()*numbers.length)]
+console.log(computerSelect);
+let lives = 5
+while(lives>0) {
+    let userInput = input.question('gusse a number----')
+    if(userInput == computerSelect) {
+        console.log('You won!')
+        break;
+    }else if(userInput>computerSelect){
+        console.log(`Your number is high. you have ${--lives} turn left`)
+    }else if(userInput<computerSelect){
+        console.log(`Your number is low. you have ${--lives} turn left`)
+    }
+    if(lives == 0){
+        console.log('Sorry you loose the game. Try next time')
+        break;
+    }
+}
