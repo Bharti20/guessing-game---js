0d952a431ad405fba708fa54362b42f59f67496e
Bharti kumari <bhartikumari20@navgurukul.org>
2021-07-03 20:02:20 +0530

    add welcome statment

--- guessing _game.js
+++ guessing _game.js
@@ -1,3 +1,5 @@
+
+console.log('------------------ Welcome to the Guessing Game! -----------------')
 const input = require('readline-sync')
 let numbers = [1,2,3,4,5,6,7,8,9,10]
 let computerSelect = numbers[Math.floor(Math.random()*numbers.length)]
